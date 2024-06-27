6475cb268e1bdf92dd00bb17c0b67c16762b7bea
Fersh5 <dev_fer5@outlook.com>
2024-06-27 01:02:48 -0600

    add proyect csv

---
+++ app/main.py
@@ -0,0 +1,23 @@
+import utils
+import charts
+import read_csv
+
+def run():
+    country = input('Type Country: ')
+    if country == 'Densidad':
+        labels,values=utils.get_axes_pie()
+        charts.generate_pie_chart(labels,values)
+    else: 
+        result = utils.get_dict_country(country)
+        if len(result)>0:
+            keys, values = utils.get_population(result)
+            charts.generate_bar_chart(keys,values)
+        else:
+            print('Ciudad no encontrada')
+
+if __name__=='__main__':
+    run()
+
+
+
+
