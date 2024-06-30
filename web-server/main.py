483a1a04df2594d9cf60d8db98d6c72d8796dfb0
Fersh5 <dev_fer5@outlook.com>
2024-06-30 01:09:08 -0600

    Lesvatando nuestro propio servidor con uvicorn y fastapi

---
+++ web-server/main.py
@@ -0,0 +1,25 @@
+import store
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+app = FastAPI()
+
+@app.get('/')
+def get_list():
+    return [1,2,3]
+
+@app.get('/contact', response_class=HTMLResponse)
+def get_list():
+    return '''
+    <h1>Hola bienvenido a esta pagina</h1>
+    <p>Este es un parrafo</p>
+    '''
+
+
+def run():
+    store.get_categories()
+
+
+if __name__=='__main__':
+    run()
+    
